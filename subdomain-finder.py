36ab37e7c02d666f21f5f985e7f7deb3f7be245a
av3sk77 <70233417+av3sk77@users.noreply.github.com>
2020-08-31 23:12:46 +0530

    Add files via upload

---
+++ subdomain-finder.py
@@ -0,0 +1,70 @@
+print('''
+███████╗██╗   ██╗██████╗ ██████╗  ██████╗ ███╗   ███╗ █████╗ ██╗███╗   ██╗    ███████╗██╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔════╝██║   ██║██╔══██╗██╔══██╗██╔═══██╗████╗ ████║██╔══██╗██║████╗  ██║    ██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗
+███████╗██║   ██║██████╔╝██║  ██║██║   ██║██╔████╔██║███████║██║██╔██╗ ██║    █████╗  ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+╚════██║██║   ██║██╔══██╗██║  ██║██║   ██║██║╚██╔╝██║██╔══██║██║██║╚██╗██║    ██╔══╝  ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+███████║╚██████╔╝██████╔╝██████╔╝╚██████╔╝██║ ╚═╝ ██║██║  ██║██║██║ ╚████║    ██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║
+╚══════╝ ╚═════╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+''')
+
+from pathlib import Path
+import dns.resolver
+import argparse
+import requests
+import threading
+import sys
+
+print('Made By- Aves Ahmed Khan')
+print('')
+print('If You Have Any Query PM me at:')
+print('Twitter  - https://twitter.com/av3sk77')
+print('LinkedIn - https://www.linkedin.com/in/aves-ahmed-khan-b835a7168/')
+print('')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", dest="out", help='Output File [Ex. -o "found.txt"]')
+parser.add_argument("-s", dest="subdomain", help='SubDomains List [Ex. -s "/root/Desktop/SubDomains.txt"]')
+
+required = parser.add_argument_group('Required Arguments')
+required.add_argument("-d", dest="domain", help='Domain Name [Ex. -d google.com]', required=True)
+
+args = parser.parse_args()
+out_file = args.out
+domain_name = args.domain
+
+if args.subdomain is None:
+    sub_domains = "default.txt"
+elif not Path(args.subdomain).exists() & Path(args.subdomain).is_file():
+    print("Sub-Domains File Not Found:", args.subdomain)
+    sys.exit()
+else:
+    sub_domains = args.subdomain
+
+if out_file is not None:
+    write_file = open(out_file, "w")
+
+server = dns.resolver.Resolver()
+server.nameservers = ['8.8.8.8']
+
+def check(list, domain_name):
+    try:
+        fdomain = f"{list}.{domain_name}"
+        dnsans = server.query(fdomain, 'A')
+
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers):
+        pass
+    except (dns.name.EmptyLabel, dns.name.LabelTooLong):
+        print(f"Make Sure SubDomain file is Correct")
+        sys.exit()
+    else:
+        for rdata in dnsans:
+            print(f"{fdomain} is Found")
+            if out_file is not None:
+                write_file = open(out_file, "a")
+                write_file.write(f"{fdomain}\n")
+
+with open(sub_domains, 'r') as file:
+    for list in file.read().splitlines():
+        t = threading.Thread(target=check, args=(list, domain_name,))
+        t.start()
